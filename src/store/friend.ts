4f90e9a89ce00dcf5f024f5d0ee294618fc257f8
guozw <guozw98@qq.com>
2025-03-27 19:33:04 +0800

    fix: update file src/store/friend.ts

--- src/store/friend.ts
+++ src/store/friend.ts
@@ -8,58 +8,8 @@ export interface FriendState {
   setFriendRemark: (friendID: number, remark: string) => void;
 }
 
-const mockData = [
-  {
-    id: 10001,
-    username: 'xiaoming',
-    nickname: '小明',
-    avatar_url: '',
-    remark: '',
-    gender: 1,
-    signature: '',
-    is_online: true,
-  },
-  {
-    id: 10002,
-    username: 'xiaofang',
-    nickname: '小芳',
-    avatar_url: '',
-    remark: '',
-    gender: 2,
-    signature: '',
-    is_online: true,
-  },
-  {
-    id: 10003,
-    username: 'xiaozhe123',
-    nickname: '小哲',
-    avatar_url: '',
-    remark: '',
-    gender: 1,
-    signature: '',
-    is_online: false,
-  },
-];
-
-function genMockFriends(startId: number, num: number) {
-  const data = [];
-  for (let idx = 0; idx < num; idx++) {
-    data.push({
-      id: startId + idx,
-      username: `mockfriend-${idx}`,
-      nickname: `好友${idx}`,
-      avatar_url: '',
-      gender: idx % 2,
-      signature: '',
-      remark: `这是备注111111111111111111111111111111111111111111-${idx}`,
-      is_online: idx % 2 == 0,
-    });
-  }
-  return data;
-}
-
 const createFriendSlice: StateCreator<FriendState> = (set) => ({
-  friendStore: { friends: [...mockData, ...genMockFriends(20000, 16)] },
+  friendStore: { friends: [] },
   fetchFriendInfo: async () => {
     try {
       const { data } = await friendApi.listFriend();
